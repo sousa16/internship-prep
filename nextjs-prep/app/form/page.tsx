65705d24a7dcfb55616ebea4452db9d91b53c5b4
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-06-24 12:38:27 +0100

    fix: fixed testid for navigation buttons

--- nextjs-prep/app/form/page.tsx
+++ nextjs-prep/app/form/page.tsx
@@ -32,7 +32,9 @@ export default function FormPage() {
           href="/"
           rel="noopener noreferrer">
           <p className="text-lg fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            <code className="font-mono font-bold">Home</code>
+            <code data-testid="home" className="font-mono font-bold">
+              Home
+            </code>
           </p>
         </a>
         <a
@@ -40,7 +42,9 @@ export default function FormPage() {
           href="/"
           rel="noopener noreferrer">
           <p className="text-lg fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            <code className="font-mono font-bold">Login</code>
+            <code data-testid="login" className="font-mono font-bold">
+              Login
+            </code>
           </p>
         </a>
       </div>
--- nextjs-prep/app/page.tsx
+++ nextjs-prep/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
           href="/login"
           rel="noopener noreferrer">
           <p className="text-lg fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            <code className="font-mono font-bold">Login</code>
+            <code data-testid="login" className="font-mono font-bold">
+              Login
+            </code>
           </p>
         </a>
         <a
@@ -15,7 +17,9 @@ export default function Home() {
           href="/form"
           rel="noopener noreferrer">
           <p className="text-lg fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            <code className="font-mono font-bold">Form</code>
+            <code data-testid="form" className="font-mono font-bold">
+              Form
+            </code>
           </p>
         </a>
       </div>
