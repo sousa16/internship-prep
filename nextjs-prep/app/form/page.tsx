4a7744bf8969115e2449c9b8f9e0f7dcd5e104a9
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-06-24 14:01:23 +0100

    fix: fixed form page navigation

--- nextjs-prep/app/form/page.tsx
+++ nextjs-prep/app/form/page.tsx
@@ -39,7 +39,7 @@ export default function FormPage() {
         </a>
         <a
           className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-          href="/"
+          href="/login"
           rel="noopener noreferrer">
           <p className="text-lg fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             <code data-testid="login" className="font-mono font-bold">
