90bf8797cdc4f4032a2510e865f1b636b097d8e4
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-06-24 12:55:07 +0100

    fix: fixed homepage tests to use getByRole

--- nextjs-prep/tests/home.spec.ts
+++ nextjs-prep/tests/home.spec.ts
@@ -2,12 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('login button', async ({ page }) => {
     await page.goto('http://localhost:3000/');
-    await page.getByTestId('login').click();
+    await page.getByRole('link', { name: 'Login' }).click();
     await expect(page).toHaveURL('http://localhost:3000/login');
 });
 
 test('form button', async ({ page }) => {
     await page.goto('http://localhost:3000/');
-    await page.getByTestId('form').click();
+    await page.getByRole('link', { name: 'Form' }).click();
     await expect(page).toHaveURL('http://localhost:3000/form');
 });
