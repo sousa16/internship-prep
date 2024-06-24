4063174452a31286337462109c8cdd5fdecb7d21
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-06-24 13:25:44 +0100

    feat: added login page tests

---
+++ nextjs-prep/tests/login.spec.ts
@@ -0,0 +1,67 @@
+import { test, expect } from '@playwright/test';
+
+test('home button', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+  await page.getByRole('link', { name: 'Home' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/');
+});
+
+test('form button', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+  await page.getByRole('link', { name: 'Form' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/form');
+});
+
+test('successful login', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+  await page.fill('#username', 'admin');
+  await page.fill('#password', 'admin');
+
+  // Enabling alert handling
+  page.on('dialog', async (dialog) => {
+    expect(dialog.type()).toBe('alert');
+    expect(dialog.message()).toContain('Login successful!');
+    await dialog.accept();
+  });
+
+  await page.click('button[type="submit"]');
+});
+
+test('wrong password', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+  await page.fill('#username', 'user');
+  await page.fill('#password', 'wrongpassword');
+
+  // Enabling alert handling
+  page.on('dialog', async (dialog) => {
+    expect(dialog.type()).toBe('alert');
+    expect(dialog.message()).toContain('Invalid credentials');
+    await dialog.accept();
+  });
+  
+  await page.click('button[type="submit"]');
+});
+
+test('empty username', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+  await page.fill('#password', 'admin');
+
+  await page.click('button[type="submit"]'); // Click submit with empty fields
+
+  // Check if the first input (username) is focused
+  // This indicates that the browser's validation is prompting the user to fill out this field
+  const isUsernameFocused = await page.evaluate(() => document.activeElement.id === 'username');
+  expect(isUsernameFocused).toBeTruthy();
+});
+
+test('empty password', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+  await page.fill('#username', 'admin');
+
+  await page.click('button[type="submit"]'); // Click submit with empty fields
+
+  // Check if the second input (password) is focused
+  // This indicates that the browser's validation is prompting the user to fill out this field
+  const isPasswordFocused = await page.evaluate(() => document.activeElement.id === 'password');
+  expect(isPasswordFocused).toBeTruthy();
+});
